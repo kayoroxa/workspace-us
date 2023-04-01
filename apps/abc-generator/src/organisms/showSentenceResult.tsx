01c1c56d454bd97e2c29a0650b4f0c439bac3f1c
Caio <kayoroxa@gmail.com>
2023-04-01 04:19:35 -0300

    fix css

--- apps/abc-generator/src/organisms/showSentenceResult.tsx
+++ apps/abc-generator/src/organisms/showSentenceResult.tsx
@@ -3,8 +3,8 @@ import useSentence from '@/hooks/useSentence'
 export default function ShowSentenceResult() {
   const { sentence, rawSentence, sentenceWithKey } = useSentence()
   return (
-    <div className="py-6 px-4 text-3xl w-full text-center mb-10">
-      {sentence.length === 0 && <div>...</div>}
+    <div className="py-6 px-4 text-3xl w-full text-center mb-10 h-28">
+      {/* {sentence.length === 0 && <div>...</div>} */}
       {sentence.length > 0 && (
         <div className="flex gap-2 justify-center items-center">
           {rawSentence
