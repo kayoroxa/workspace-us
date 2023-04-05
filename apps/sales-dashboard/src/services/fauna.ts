cada6ccd39f3fb0259386983e867db9e5f36440b
Caio <kayoroxa@gmail.com>
2023-04-05 18:33:43 -0300

    s

--- apps/sales-dashboard/src/services/fauna.ts
+++ apps/sales-dashboard/src/services/fauna.ts
@@ -1,6 +1,7 @@
 import { Client } from 'faunadb'
 
 export const faunaClient = new Client({
-  secret: process.env.FAUNADB_KEY || '',
+  secret: 'fnAFAw9AWBACV-WkTeV0VwWkFPDl895T6-G7vJhF',
+  // secret: process.env.FAUNADB_KEY || '',
   // domain: process.env.FAUNA_DOMAIN,
 })
