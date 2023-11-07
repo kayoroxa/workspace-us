df330a2fe55635d92992a81d4be6a56a649e6ee7
your Name <kayoroxa@gmail.com>
2023-11-07 13:52:43 -0300

    fix duplicates

--- apps/sales-dashboard/src/pages/index.tsx
+++ apps/sales-dashboard/src/pages/index.tsx
@@ -13,13 +13,16 @@ function diferenceDays(date1: Date, date2: Date) {
 }
 
 export default function Home() {
-  const { data: eventsBuy, isLoading } = useQuery<_EventBuy[]>(
+  let { data: eventsBuy, isLoading } = useQuery<_EventBuy[]>(
     ['events-buy'],
     () => fetch('/api/events-buy').then(res => res.json()),
     {
       refetchInterval: 3 * 60 * 1000,
     }
   )
+
+  if (isLoading) return <div>Loading...</div>
+  if (!eventsBuy) return <div>Error {JSON.stringify(eventsBuy)}</div>
 
   function convertEvent(event: _EventBuy) {
     if (event.event === 'PURCHASE_APPROVED') return 'Comprou 💹'
@@ -33,6 +36,33 @@ export default function Home() {
     else return event.event
   }
 
+  function emojiHistoric(historic: string) {
+    historic = historic.replace(/PURCHASE_APPROVED/g, '💹')
+    historic = historic.replace(/PURCHASE_COMPLETE/g, '✅💹')
+    historic = historic.replace(/PURCHASE_OUT_OF_SHOPPING_CART/g, '❌🛒')
+    historic = historic.replace(/PURCHASE_CANCELED/g, '❌💳')
+    historic = historic.replace(/PURCHASE_PROTEST/g, '😡')
+    historic = historic.replace(/PURCHASE_BILLET_PRINTED/g, '⏱✉')
+    return historic
+  }
+
+  if (typeof eventsBuy?.map !== 'function') {
+    return <div>Error {JSON.stringify(eventsBuy)}</div>
+  }
+
+  eventsBuy = eventsBuy.reduce((acc, event) => {
+    const existIndex = acc.findIndex(v => v.email === event.email)
+    if (existIndex > -1) {
+      // acc.splice(existIndex, 1) // remove
+      acc[existIndex] = {
+        ...acc[existIndex],
+        historic: emojiHistoric(event.event + ' -> ' + acc[existIndex].event),
+      }
+      return acc
+    }
+    return [...acc, event]
+  }, [] as _EventBuy[])
+
   return (
     <div className="w-full min-h-screen bg-zinc-800 text-zinc-100 flex flex-col items-center gap-5 p-10 ">
       {typeof eventsBuy?.map === 'function' &&
@@ -41,6 +71,12 @@ export default function Home() {
             key={event.date}
             className="flex gap-10 bg-zinc-700 p-4 w-fit rounded-lg"
           >
+            {event.historic && event.historic.length > 0 && (
+              <section>
+                <h1>Historic:</h1>
+                <h1>{event.historic}</h1>
+              </section>
+            )}
             <section>
               <h1>Nome:</h1>
               <h4>{event.buyerName}</h4>
@@ -50,12 +86,14 @@ export default function Home() {
               <h4>{event.phone}</h4>
             </section>
             <section>
-              <h1>Evento:</h1>
-              <h1>{convertEvent(event)}</h1>
-            </section>
-            <section>
               <h1>Email:</h1>
               <h1>{event.email}</h1>
+            </section>
+            <section className="bg-zinc-600 px-3">
+              <h1>Evento:</h1>
+              <h1>
+                <strong>{convertEvent(event)}</strong>
+              </h1>
             </section>
             <section>
               <h1>Dias:</h1>
@@ -74,6 +112,7 @@ export default function Home() {
                 })}
               </h2>
             </section>
+
             <section className="flex justify-center items-center">
               <Toggle
                 id={event.email}
