dd328135542459f3ad5f44a58a40589710ec02a6
kayoroxa <kayoroxa@gmail.com>
2024-10-21 18:36:47 -0300

    api public

--- apps/sales-dashboard/next.config.js
+++ apps/sales-dashboard/next.config.js
@@ -1,6 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // Aplica a política para todas as rotas
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: `
+              default-src 'self'; 
+              connect-src 'self' https://inovasy-sells-dashboard.netlify.app;
+              script-src 'self'; 
+              style-src 'self'; 
+              img-src 'self'; 
+              frame-src 'self';
+            `
+              .replace(/\s{2,}/g, ' ')
+              .trim(),
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
